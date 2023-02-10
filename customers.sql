7a9ceb179ec74fc7a09409b6d8046ad7e3597c08
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2023-02-10 14:02:29 +0000

    Add customers.sql

---
+++ customers.sql
@@ -0,0 +1,20 @@
+SELECT
+  customers.id AS customer_id,
+  name,
+  email,
+  MIN(orders.created_at) AS first_order_at,
+  COUNT(DISTINCT orders.id) AS number_of_orders
+FROM
+  `analytics-engineers-club.coffee_shop.customers` AS customers
+LEFT JOIN
+  `analytics-engineers-club.coffee_shop.orders` AS orders
+ON
+  customers.id = orders.customer_id
+GROUP BY
+  customers.id,
+  name,
+  email
+ORDER BY
+  first_order_at
+LIMIT
+  5
