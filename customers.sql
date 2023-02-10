e3703aa4bec78f8b6513cc34127bfdd1920de96c
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2023-02-10 14:07:27 +0000

    Add last_order_created_at field to customers.sql

--- customers.sql
+++ customers.sql
@@ -2,7 +2,8 @@ SELECT
   customers.id AS customer_id,
   name,
   email,
-  MIN(orders.created_at) AS first_order_at,
+  MIN(orders.created_at) AS first_order_created_at,
+  MAX(orders.created_at) AS last_order_created_at,
   COUNT(DISTINCT orders.id) AS number_of_orders
 FROM
   `analytics-engineers-club.coffee_shop.customers` AS customers
