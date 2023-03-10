7d15e159765f9c801f0b9819bf745f859761e22e
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2023-03-10 14:09:52 +0000

    Add tests for pages and sessions models

--- coffee_shop_steven/models/marts/sales/sessions.sql
+++ coffee_shop_steven/models/marts/sales/sessions.sql
@@ -4,11 +4,10 @@
 
 SELECT
     session_id,
-    visitor_id,
     user_id,
     MIN(viewed_at) AS session_start_time,
     MAX(viewed_at) AS session_end_time,
     COUNT(DISTINCT page_viewed) AS number_of_pages_visited,
     LOGICAL_OR(page_viewed = "order-confirmation") AS purchased
 FROM {{ref('pageviews')}} AS pageviews
-GROUP BY session_id, visitor_id, user_id
+GROUP BY session_id, user_id
